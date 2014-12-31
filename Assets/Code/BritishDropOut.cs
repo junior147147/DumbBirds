3e7036bb9d7562f2b4622e5752b8e36ebf874fbf
junior147147 <junior147147@github.com>
2014-12-31 13:42:48 -0700

    Basic Layout

---
+++ Assets/Code/BritishDropOut.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BritishDropOut : MonoBehaviour {
+
+//	void Movement();
+	public float Movementspeed = 4;
+	//public float Movement_x;
+
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Movement ();
+	}
+
+	void Movement()
+	{
+
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			transform.Translate(Vector2.right * Movementspeed * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0,0);
+		}
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			transform.Translate(Vector2.right * Movementspeed * Time.deltaTime);
+			transform.eulerAngles = new Vector2(0,180);
+		}
+
+
+
+	}
+
+
+
+}
