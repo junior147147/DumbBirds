1120ab433ec68467b5e842edb293b1d702890c4b
junior147147 <junior147147@github.com>
2015-01-01 15:40:19 -0700

    finishing up

--- Assets/Code/BritishDropOut.cs
+++ Assets/Code/BritishDropOut.cs
@@ -5,7 +5,12 @@ public class BritishDropOut : MonoBehaviour {
 
 //	void Movement();
 	public float Movementspeed = 4;
-	//public float Movement_x;
+	public float Jumpspeed = 300f;
+	public Transform grounededEnd , lineStart, lineEnd;
+	public bool grounded = false;
+	public bool dumbBird = false;
+	public bool interact = false;
+	RaycastHit2D whatIHit;
 
 	void Start () 
 	{
@@ -15,27 +20,67 @@ public class BritishDropOut : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+		Raycasting ();
 		Movement ();
+		Jump ();
+		Attack ();
 	}
+
+	void Raycasting ()  
+	{
+		Debug.DrawLine (lineStart.position, lineEnd.position, Color.black);
+		Debug.DrawLine (this.transform.position, grounededEnd.position, Color.black);
+
+		grounded = Physics2D.Linecast (this.transform.position, grounededEnd.position, 1 << LayerMask.NameToLayer ("Ground"));
+
+
+
+		if(Physics2D.Linecast(lineStart.position, lineEnd.position, 1<<LayerMask.NameToLayer("DumbBird")))
+		  {
+			whatIHit = Physics2D.Linecast(lineStart.position, lineEnd.position, 1<<LayerMask.NameToLayer("DumbBird"));
+			interact =  true;
+		  }
+		else
+		{
+			interact = false;
+		}
+	}
+
+
+
 
 	void Movement()
 	{
 
-		if (Input.GetKey (KeyCode.D)) 
+		if (Input.GetKey (KeyCode.RightArrow)) 
 		{
 			transform.Translate(Vector2.right * Movementspeed * Time.deltaTime);
 			transform.eulerAngles = new Vector2(0,0);
 		}
-		if (Input.GetKey (KeyCode.A)) 
+		if (Input.GetKey (KeyCode.LeftArrow)) 
 		{
 			transform.Translate(Vector2.right * Movementspeed * Time.deltaTime);
 			transform.eulerAngles = new Vector2(0,180);
 		}
 
-
-
 	}
 
+	void Jump ()
+	{
+		
+		if (Input.GetKeyDown (KeyCode.Space) && grounded == true)
+		{
+			rigidbody2D.AddForce(Vector2.up * Jumpspeed);
+		}
+	
+	}
 
+	void Attack ()
+	{
+		if(Input.GetKeyDown (KeyCode.A) && interact == true)
+		{
+			Destroy(whatIHit.collider.gameObject);
+		}
+	}
 
 }
--- Assets/Code/DumbBird.cs
+++ Assets/Code/DumbBird.cs
@@ -3,13 +3,61 @@ using System.Collections;
 
 public class DumbBird : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+
+	public Transform sightStart, sightEnd;
+	public bool spotted = false;
+	public GameObject markTop;
+	public GameObject markBot;
+	public bool facingLeft = true;
+
+
+	void Start () 
+	{
+		InvokeRepeating ("Patrol", 0f, Random.Range(2f,6f));
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
-	
+		RayCasting ();
+		Behavior ();
 	}
+
+
+	void RayCasting()
+	{
+		Debug.DrawLine (sightStart.position, sightEnd.position, Color.black);
+		spotted = Physics2D.Linecast (sightStart.position, sightEnd.position , 1 << LayerMask.NameToLayer("BritishDropOut"));
+
+	}
+
+	void Behavior ()
+	{
+		if (spotted) 
+		{
+			markTop.SetActive(true);
+			markBot.SetActive(true);
+		}
+		else
+		{
+			markTop.SetActive(false);
+			markBot.SetActive(false);
+		}
+	}
+
+	void Patrol ()
+	{
+		facingLeft = !facingLeft;
+		//if true, set it to false, vice versa.
+		if (facingLeft)
+		{
+			transform.eulerAngles= new Vector2(0,0);
+		}
+		else
+		{
+			transform.eulerAngles= new Vector2(0,180);
+		}
+	}
+
+
 }
